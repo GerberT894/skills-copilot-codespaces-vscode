1fcdd75becd0d7963d797c937d6eab1e5ee260cd
GerberT894 <gerbert@legal.regn.net>
2025-02-11 07:15:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,81 +1,38 @@
 // create a Web server
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const template = require('./lib/template.js');
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
-const mysql = require('mysql');
+// npm install express body-parser
+// npm install nodemon
+// run the server: nodemon comments.js
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '111111',
-  database: 'opentutorials',
+// Import the express library
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create an instance of the express server
+const app = express();
+
+// To parse the body of a POST request
+app.use(bodyParser.json());
+
+// Create a list of comments
+const comments = [
+    {username: 'alice', comment: 'I love cats!'},
+    {username: 'bob', comment: 'I love dogs!'},
+];
+
+// Handle GET request to /comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
-db.connect();
 
-const app = http.createServer(function (request, response) {
-  const _url = request.url;
-  const queryData = url.parse(_url, true).query;
-  const pathname = url.parse(_url, true).pathname;
+// Handle POST request to /comments
+app.post('/comments', (req, res) => {
+    // req.body is the body of the POST request
+    const comment = req.body;
+    comments.push(comment);
+    res.json(comment);
+});
 
-  if (pathname === '/') {
-    if (queryData.id === undefined) {
-      db.query(`SELECT * FROM topic`, (err, topics) => {
-        if (err) {
-          throw err;
-        }
-        const title = 'Welcome';
-        const description = 'Hello, Node.js';
-        const list = template.list(topics);
-        const html = template.HTML(
-          title,
-          list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else {
-      db.query(`SELECT * FROM topic`, (err, topics) => {
-        if (err) {
-          throw err;
-        }
-        db.query(
-          `SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
-          [queryData.id],
-          (err2, topic) => {
-            if (err2) {
-              throw err2;
-            }
-            const title = topic[0].title;
-            const description = topic[0].description;
-            const list = template.list(topics);
-            const html = template.HTML(
-              title,
-              list,
-              `<h2>${title}</h2>${description}<p>by ${topic[0].name}</p>`,
-              `
-              <a href="/create">create</a>
-              <a href="/update?id=${queryData.id}">update</a>
-              <form action="delete_process" method="post">
-                <input type="hidden" name="id" value="${queryData.id}">
-                <input type="submit" value="delete">
-                            </form>
-                            `
-                          );
-                          response.writeHead(200);
-                          response.end(html);
-                        }
-                      );
-                    });
-                  }
-                } else {
-                  response.writeHead(404);
-                  response.end('Not found');
-                }
-              });
-              app.listen(3000);
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
