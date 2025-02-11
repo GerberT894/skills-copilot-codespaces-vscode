2d9740164f0bca2bdf6d19bfbfdd55d9a21520fa
GerberT894 <gerbert@legal.regn.net>
2025-02-11 07:00:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,82 @@
+// create a Web server
+// create a Web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+const template = require('./lib/template.js');
+const path = require('path');
+const sanitizeHtml = require('sanitize-html');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '111111',
+  database: 'opentutorials',
+});
+db.connect();
+
+const app = http.createServer(function (request, response) {
+  const _url = request.url;
+  const queryData = url.parse(_url, true).query;
+  const pathname = url.parse(_url, true).pathname;
+
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      db.query(`SELECT * FROM topic`, (err, topics) => {
+        if (err) {
+          throw err;
+        }
+        const title = 'Welcome';
+        const description = 'Hello, Node.js';
+        const list = template.list(topics);
+        const html = template.HTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      });
+    } else {
+      db.query(`SELECT * FROM topic`, (err, topics) => {
+        if (err) {
+          throw err;
+        }
+        db.query(
+          `SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,
+          [queryData.id],
+          (err2, topic) => {
+            if (err2) {
+              throw err2;
+            }
+            const title = topic[0].title;
+            const description = topic[0].description;
+            const list = template.list(topics);
+            const html = template.HTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}<p>by ${topic[0].name}</p>`,
+              `
+              <a href="/create">create</a>
+              <a href="/update?id=${queryData.id}">update</a>
+              <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${queryData.id}">
+                <input type="submit" value="delete">
+                            </form>
+                            `
+                          );
+                          response.writeHead(200);
+                          response.end(html);
+                        }
+                      );
+                    });
+                  }
+                } else {
+                  response.writeHead(404);
+                  response.end('Not found');
+                }
+              });
+              app.listen(3000);
